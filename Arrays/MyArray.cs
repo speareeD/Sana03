6ec3ca1b00d825bfe8b7087a49a0e5ac0b0f86bd
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-03 10:30:54 +0200

    transfered all the action from program to MyArray class and made own method to get them

--- Arrays/MyArray.cs
+++ Arrays/MyArray.cs
@@ -13,6 +13,7 @@ namespace Arrays
         protected double MaxElementIndex = int.MinValue;
         protected double MaxAbsElement = int.MinValue;
         protected int PositiveElementsIndexSum = 0;
+        protected int IntergersCount = 0;
         protected double[] Array;
 
         public MyArray(double[] array)
@@ -27,6 +28,51 @@ namespace Arrays
                 if(Array[i] < 0) NegativeSum += Array[i];
             }
             Console.WriteLine($"Sum of negative number: {NegativeSum}");
+        }
+
+        public void GetMinElement()
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] < MinElement) MinElement = Array[i];
+            }
+            Console.WriteLine($"Minimum number: {MinElement}");
+        }
+
+        public void GetMaxIndex()
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] > MaxElementIndex) MaxElementIndex = i;
+            }
+            Console.WriteLine($"Index of max element: {MaxElementIndex}");
+        }
+
+        public void GetMaxAbsElement()
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Math.Abs(Array[i]) > MaxAbsElement) MaxAbsElement = Array[i];
+            }
+            Console.WriteLine($"Max absolute element: {MaxAbsElement}");
+        }
+
+        public void GetPositiveIndexSum()
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Math.Abs(Array[i]) > 0) PositiveElementsIndexSum += i;
+            }
+            Console.WriteLine($"Sum of positive number index: {PositiveElementsIndexSum}");
+        }
+
+        public void GetAmountIntegers()
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] % 1.0 == 0.0) IntergersCount += 1;
+            }
+            Console.WriteLine($"Amount of integers: {IntergersCount}");
         }
     }
 }
--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Sana03
+﻿using Arrays;
+
+namespace Sana03
 {
     class Program
     {
@@ -6,35 +8,16 @@
         {
             int n;
             double[] array = new double[10];
-            double negativeSum = 0;
-            double minElement = int.MaxValue;
-            double maxElementIndex = int.MinValue;
-            double maxAbsElement = int.MinValue;
-
             Random rand = new Random();
             Console.Write("Enter amount of elements in array: ");
             n = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
-                array[i] = rand.Next(-10, 11) + rand.Next(0, 100) / 100.0;
+                array[i] = Math.Round(rand.Next(-10, 10) + rand.Next(0, 100) / 100.0, 2);
                 Console.Write(array[i] + " ");
-                if (array[i] < 0)
-                {
-                    negativeSum += array[i];
-                }
-                if (array[i] < minElement)
-                {
-                    minElement = array[i];
-                }
-                if (array[i] > maxElementIndex)
-                {
-                    maxElementIndex = i;
-                }
-                if (Math.Abs(array[i]) > maxAbsElement)
-                {
-                    maxAbsElement = array[i];
-                }
             }
+            Console.WriteLine();
+            MyArray myArray = new MyArray(array);
         }
     }
 }
