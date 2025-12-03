1f15cfae6041c9e9202eb5585daad9483b16a7c9
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-03 09:47:35 +0200

    Added array, random, input amount of numbers in array, fill array and show the array in console

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -4,7 +4,16 @@
     {
         public static void Main(string[] args)
         {
-
+            int n;
+            double[] array = new double[10];
+            Random rand = new Random();
+            Console.Write("Enter amount of elements in array: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rand.Next(-10, 11) + rand.Next(0, 100) / 100.0;
+                Console.Write(array[i] + " ");
+            }
         }
     }
 }
