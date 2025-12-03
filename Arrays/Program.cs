e78d6ba3f1eaecf40e909ff123340947a4ddadaa
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-03 09:56:00 +0200

    Changed sum of positives to negatives. Found index of max element and max absoulte number

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,8 +6,10 @@
         {
             int n;
             double[] array = new double[10];
-            double positiveSum = 0;
+            double negativeSum = 0;
             double minElement = int.MaxValue;
+            double maxElementIndex = int.MinValue;
+            double maxAbsElement = int.MinValue;
 
             Random rand = new Random();
             Console.Write("Enter amount of elements in array: ");
@@ -16,19 +18,23 @@
             {
                 array[i] = rand.Next(-10, 11) + rand.Next(0, 100) / 100.0;
                 Console.Write(array[i] + " ");
-                if (array[i] > 0)
+                if (array[i] < 0)
                 {
-                    positiveSum += array[i];
+                    negativeSum += array[i];
                 }
                 if (array[i] < minElement)
                 {
                     minElement = array[i];
                 }
-
+                if (array[i] > maxElementIndex)
+                {
+                    maxElementIndex = i;
+                }
+                if (Math.Abs(array[i]) > maxAbsElement)
+                {
+                    maxAbsElement = array[i];
+                }
             }
         }
-
-
     }
-}
 }
