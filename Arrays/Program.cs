ab0c29aee90a58b1d2f927282ff1bb592960247d
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-03 09:51:26 +0200

    Find sum of positive number in array and min element of array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,6 +6,9 @@
         {
             int n;
             double[] array = new double[10];
+            double positiveSum = 0;
+            double minElement = int.MaxValue;
+
             Random rand = new Random();
             Console.Write("Enter amount of elements in array: ");
             n = int.Parse(Console.ReadLine());
@@ -13,7 +16,19 @@
             {
                 array[i] = rand.Next(-10, 11) + rand.Next(0, 100) / 100.0;
                 Console.Write(array[i] + " ");
+                if (array[i] > 0)
+                {
+                    positiveSum += array[i];
+                }
+                if (array[i] < minElement)
+                {
+                    minElement = array[i];
+                }
+
             }
         }
+
+
     }
+}
 }
