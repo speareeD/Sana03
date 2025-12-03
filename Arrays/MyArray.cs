720d1d374fff57ede267291e1a708f6f248834bb
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-03 10:12:50 +0200

    added necessery variables and protected them. Added GetNegativeSum method

--- Arrays/MyArray.cs
+++ Arrays/MyArray.cs
@@ -8,5 +8,25 @@ namespace Arrays
 {
     internal class MyArray
     {
+        protected double NegativeSum = 0;
+        protected double MinElement = int.MaxValue;
+        protected double MaxElementIndex = int.MinValue;
+        protected double MaxAbsElement = int.MinValue;
+        protected int PositiveElementsIndexSum = 0;
+        protected double[] Array;
+
+        public MyArray(double[] array)
+        {
+            Array = array;
+        }
+
+        public void GetNegativeSum()
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if(Array[i] < 0) NegativeSum += Array[i];
+            }
+            Console.WriteLine($"Sum of negative number: {NegativeSum}");
+        }
     }
 }
